9cb25cbeab200947dd765d4a314e445f367e1fcb
piyushpawar079 <piyushpawar079@gmail.com>
2025-02-28 00:27:57 +0530

    CORS Error resolved

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,10 +8,7 @@ import orderRouter from './routes/order.routes.js'
 
 const app = express();
 
-app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true
-}))
+app.use(cors())
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
