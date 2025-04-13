7915399593b45361a1f1d2f72385c09fe69874a5
piyushpawar079 <piyushpawar079@gmail.com>
2025-04-13 16:36:56 +0530

    Save

--- backend/src/middlewares/multer.middleware.js
+++ backend/src/middlewares/multer.middleware.js
@@ -3,9 +3,11 @@ import multer from 'multer'
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
+      console.log(process.cwd());
       cb(null, '../src/public/temp')
     },
     filename: function (req, file, cb) {
+      console.log(process.cwd());
       cb(null, Date.now() + '-' + file.originalname)
     }
   })
